d061c8ff868e6e5a5a5ec7d3f7e0749658862225
Ciprian <cozma_ciprian19@yahoo.com>
2023-06-17 22:49:42 +0300

    updated form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [selectOptions, setSelectOptions] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
+    if (!description) return;
+
+    const newItem = {
+      description,
+      selectOptions,
+      packed: false,
+      id: Date.now(),
+    };
+    console.log(newItem);
+    setDescription("");
+    setSelectOptions(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do we need for our trip? 🤩</h3>
-      <select>
+      <select
+        value={selectOptions}
+        onChange={(e) => setSelectOptions(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((number) => (
           <option value={number} key={number}>
             {number}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item" />
+      <input
+        type="text"
+        placeholder="Item"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );

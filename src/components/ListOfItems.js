26acf67d02a532a5a1ce4b9a69e658d42d7b5a83
Ciprian <cozma_ciprian19@yahoo.com>
2023-06-19 18:29:06 +0300

    added sorting functionality

--- src/components/ListOfItems.js
+++ src/components/ListOfItems.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 // import { items } from "../data";
 import Item from "./Item";
 
 function ListOfItems({ items, handleDelete, handleChecked }) {
+  const [sorted, setSorted] = useState("selectOptions");
+
+  let sortedItems;
+  if (sorted === "input") {
+    sortedItems = items;
+  }
+
+  if (sorted === "description") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  }
+
+  if (sorted === "packed") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
   return (
     <div className="list">
       <ul>
-        {items.map((item, idx) => (
+        {sortedItems?.map((item, idx) => (
           <Item
             key={idx}
             item={item}
@@ -15,6 +33,13 @@ function ListOfItems({ items, handleDelete, handleChecked }) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sorted} onChange={(e) => setSorted(e.target.value)}>
+          <option value={"input"}>Sort by input order</option>
+          <option value={"description"}>Sort by description</option>
+          <option value={"packed"}>Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
